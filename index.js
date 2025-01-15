2a22bbe09060685693e859a2c143275232b5fd7a
Kabash <diegohs1503@gmail.com>
2025-01-15 15:59:31 +0100

    11avo Ejercicio

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const fs = require("fs");
 
-function leerArchivoPorBloques() {
-  const archivo = "largeData.txt";
-  const stream = fs.createReadStream(archivo, {
-    encoding: "utf8",
-    highWaterMark: 1024,
-  }); // Leer en bloques de 1KB
-
-  stream.on("data", (bloque) => {
-    console.log("--- Bloque leido ---");
-    console.log(bloque);
-  });
-
-  stream.on("end", () => {
-    console.log("--- Lectura completada ---");
-  });
-
-  stream.on("error", (err) => {
-    console.error("Error al leer el archivo:", err);
+function copiarTexto() {
+  fs.readFile("source.txt", "utf-8", (error, datos) => {
+    if (error) {
+      console.error("Error leyendo source.txt");
+      return;
+    }
+    fs.writeFile("destination.txt", datos, (error) => {
+      if (error) {
+        console.error("Error escribiendo destination.txt");
+        return;
+      }
+      console.log("Datos en destination.txt escritos correctamente");
+    });
   });
 }
 
-leerArchivoPorBloques();
+copiarTexto();
