48f5a34eb31675a200d326e21e6db61fc5cbef85
Kabash <diegohs1503@gmail.com>
2025-01-15 16:25:00 +0100

    Adding readme.md

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 const fs = require("fs");
 
-function copiarConPipe() {
-  const archivoEntrada = "entrada.txt";
-  const archivoSalida = "salida.txt";
-
-  const readStream = fs.createReadStream(archivoEntrada, { encoding: "utf8" });
-  const writeStream = fs.createWriteStream(archivoSalida);
-
-  // Conectar los streams con pipe
-  readStream.pipe(writeStream);
-
-  readStream.on("error", (err) => {
-    console.error("Error al leer el archivo:", err);
-  });
-
-  writeStream.on("error", (err) => {
-    console.error("Error al escribir en el archivo:", err);
-  });
-
-  writeStream.on("finish", () => {
-    console.log(
-      `Contenido copiado exitosamente de ${archivoEntrada} a ${archivoSalida}`
-    );
-  });
+function crearCarpeta() {
+  if (fs.existsSync("backup")) {
+    console.log("La carpeta backup ya existe");
+  } else {
+    fs.mkdirSync("backup");
+    console.log("Carpeta backup creada");
+  }
 }
 
-copiarConPipe();
+crearCarpeta();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Ejercicios de Node JS
+
+## Made by Diego Hernández
+
+Cada uno de los ejercicios se podran ver y editar en el historial de Commits que se ha realizado en el repositorio, se podra retroceder y ver el funcionamiento de cada ejercicio.
+
+> En la carpeta Capturas se puede ver las evidencias del funcionamiento de cada ejercicio
