17a8034ba2c6f1aae36aca64d9944159852d4ec5
Kabash <diegohs1503@gmail.com>
2025-01-15 11:47:41 +0100

    3er Ejercicio

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function saludar() {
-  console.log("¡Hola, Mundo!");
+function suma() {
+  val = 4;
+  val2 = 5;
+  val3 = val + val2;
+  console.log("La respuesta de la suma es: " + val3);
 }
 
-saludar();
+suma();
